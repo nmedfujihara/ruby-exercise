c190c3189cb1755524edf2dc4fee28de4687ae9f
emi fujihara <emi.fujihara@nacl-med.co.jp>
2020-10-16 09:50:16 +0900

    メソッドの可視性

--- code_0440.rb
+++ code_0440.rb
@@ -7,10 +7,13 @@ class Baz1
   def protected_method2; 2; end #同じ可視性が継続される
   private
   def private_method1; 1; end
+  public
+  def public_method3; 3; end
 end
 
 p Baz1.new.public_method1
 p Baz1.new.public_method2
+p Baz1.new.public_method3
 
 p Baz1.new.protected_method1
 p Baz1.new.protected_method2
